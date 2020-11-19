68046bf52393f1e60460771db8111ae539e4cc12
Siuver <>
2020-11-19 15:32:12 +0800

    do nothing

--- src/cstar.py
+++ src/cstar.py
@@ -1,114 +1,76 @@
-MAP_CONFIG = {
-    0: {
-        'link_node': [1, 3, 6],
-        'valid': True
-    },
-    1: {
-        'link_node': [0, 4, 7],
-        'valid': True
-    },
-    2: {
-        'link_node': [4, 5],
-        'valid': True
-    },
-    3: {
-        'link_node': [0, 5, 6],
-        'valid': True
-    },
-    4: {
-        'link_node': [1, 2, 7],
-        'valid': True
-    },
-    5: {
-        'link_node': [2, 3, 8, 9],
-        'valid': True
-    },
-    6: {
-        'link_node': [0, 3, 9],
-        'valid': True
-    },
-    7: {
-        'link_node': [1, 4],
-        'valid': True
-    },
-    8: {
-        'link_node': [5],
-        'valid': True
-    },
-    9: {
-        'link_node': [5, 6],
-        'valid': True
-    },
-}
-
-node2route = {}
+import map
 
 
-def all_route_to_node(s, e, visited, path):
-    visited[s] = True
-    path.append(s)
+class Cstar:
+    def __init__(self, mapConfig):
+        self.mapConfig = mapConfig
 
-    if s == e:
-        if e not in node2route:
-            node2route[e] = []
-        node2route[e].append(path[:])
-    else:
-        nodeConfig = MAP_CONFIG[s]
-        for linkNode in nodeConfig['link_node']:
-            if visited[linkNode] == False and nodeConfig['valid'] == True:
-                all_route_to_node(linkNode, e, visited, path)
+    def all_route(self, s, e, visited, path, node2route):
+        visited[s] = True
+        path.append(s)
 
-    path.pop()
-    visited[s] = False
+        if s == e:
+            if e not in node2route:
+                node2route[e] = []
+            node2route[e].append(path[:])
+        else:
+            nodeConfig = self.mapConfig[s]
+            for linkNode in nodeConfig['link_node']:
+                if visited[linkNode] == False and nodeConfig['valid'] == True:
+                    self.all_route(linkNode, e, visited, path, node2route)
 
+        path.pop()
+        visited[s] = False
 
-def checkRouteValid(route):
-    valid = True
-    lastNode = -1
+    def checkRouteValid(self, route):
+        valid = True
+        lastNode = -1
 
-    closedNode = []
-    routeLength = len(route)
+        closedNode = []
+        routeLength = len(route)
 
-    for i in range(routeLength):
-        passNode = route[i]
-        if passNode in closedNode:
-            valid = False
-            break
+        for i in range(routeLength):
+            passNode = route[i]
+            if passNode in closedNode:
+                valid = False
+                break
 
-        closedNode.append(valid)
-        if lastNode >= 0:
-            for linkNode in MAP_CONFIG[lastNode]['link_node']:
-                if linkNode not in closedNode:
-                    closedNode.append(linkNode)
+            closedNode.append(valid)
+            if lastNode >= 0:
+                for linkNode in self.mapConfig[lastNode]['link_node']:
+                    if linkNode not in closedNode:
+                        closedNode.append(linkNode)
 
-        lastNode = passNode
+            lastNode = passNode
 
-    return valid
+        return valid
 
+    def cal_longest_route(self, startNode):
+        longest_route = []
 
-def cal_longest_route():
-    longest_route = []
+        node2route = {}
+        for node in self.mapConfig.keys():
+            visited = [False] * len(self.mapConfig.keys())
+            path = []
+            self.all_route(startNode, node, visited, path, node2route)
 
-    for node in MAP_CONFIG.keys():
-        visited = [False] * len(MAP_CONFIG.keys())
-        path = []
-        all_route_to_node(0, node, visited, path)
+        for node, routes in node2route.items():
+            for route in routes:
+                routeLength = len(route)
 
-    for node, routes in node2route.items():
-        for route in routes:
-            routeLength = len(route)
+                if routeLength < len(longest_route):
+                    continue
 
-            if routeLength < len(longest_route):
-                continue
+                if self.checkRouteValid(route):
+                    longest_route = route
 
-            if checkRouteValid(route):
-                longest_route = route
-
-    return longest_route
+        return longest_route
 
 
 def main():
-    print(cal_longest_route())
+    cstar = Cstar(map.MAP_CONFIG)
+
+    print(cstar.cal_longest_route(0))
 
 
 if __name__ == "__main__":
---
+++ src/map.py
@@ -0,0 +1,12 @@
+MAP_CONFIG = {
+    0: { 'link_node': [1, 3, 6], 'valid': True },
+    1: { 'link_node': [0, 4, 7], 'valid': True },
+    2: { 'link_node': [4, 5], 'valid': True },
+    3: { 'link_node': [0, 5, 6], 'valid': True },
+    4: { 'link_node': [1, 2, 7], 'valid': True },
+    5: { 'link_node': [2, 3, 8, 9], 'valid': True },
+    6: { 'link_node': [0, 3, 9], 'valid': True },
+    7: { 'link_node': [1, 4], 'valid': True },
+    8: { 'link_node': [5], 'valid': True },
+    9: { 'link_node': [5, 6], 'valid': True },
+}
